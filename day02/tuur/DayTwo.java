75f710dc94092cd0470156c19432eb755d31cf22
Tuur Bressinck <bressinck.tuur@gmail.com>
2024-12-02 18:15:49 +0100

    Day 2: slightly simpler

--- day02/tuur/DayTwo.java
+++ day02/tuur/DayTwo.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,7 +30,6 @@ public class DayTwo {
 	}
 	
 	private static boolean validReportPartTwo(List<Integer> report) {
-		
 		if (validReportPartOne(report)) {
 			return true;
 		}
@@ -46,17 +46,15 @@ public class DayTwo {
 	}
 
 	private static boolean validReportPartOne(List<Integer> report) {
-		boolean increasing = report.get(0) - report.get(1) < 0;
+		if (report.get(0) > report.get(1)) {
+			Collections.reverse(report); // list always in increasing order
+		}
 
 		for (int i = 1; i < report.size(); i++) {
-			int a = report.get(i-1);
+			int a = report.get(i - 1);
 			int b = report.get(i);
-			if (Math.abs(a - b) < 1 || Math.abs(a-b) > 3) {
-				return false;
-			}
-			if (increasing && a - b > 0) {
-				return false;
-			} else if (!increasing && a - b < 0) {
+			
+			if (b - a < 1 || b - a > 3) {
 				return false;
 			}
 		}
