8294701eb62ecbcba5fe4936f9bd3f65fb5416ed
Tuur Bressinck <bressinck.tuur@gmail.com>
2024-12-04 11:55:40 +0100

    Day 4: Format

--- day04/tuur/DayFour.java
+++ day04/tuur/DayFour.java
@@ -9,9 +9,9 @@ import java.util.Map;
 import java.util.Objects;
 
 public class DayFour {
-	
-	private static final Character[] XMAS = new Character[] {'X' ,'M', 'A', 'S'};
-	
+
+	private static final Character[] XMAS = new Character[] { 'X', 'M', 'A', 'S' };
+
 	public static void main(String[] args) throws Exception {
 		List<String> lines = Files.readAllLines(Paths.get(DayFour.class.getResource("input-day04.txt").toURI()));
 
@@ -25,7 +25,7 @@ public class DayFour {
 
 	}
 
-	public static long partOne(Map<Coord,Character> map, int sizeX, int sizeY) {
+	public static long partOne(Map<Coord, Character> map, int sizeX, int sizeY) {
 		long count = 0l;
 		for (int x = 0; x < sizeX; x++) {
 			for (int y = 0; y < sizeY; y++) {
@@ -38,14 +38,14 @@ public class DayFour {
 		return count;
 	}
 
-	public static long wordSearch(Map<Coord,Character> map, int sizeX, int sizeY, final Coord start) {
+	public static long wordSearch(Map<Coord, Character> map, int sizeX, int sizeY, final Coord start) {
 		long count = 0l;
-		for(Coord direction : directions() ) {
+		for (Coord direction : directions()) {
 			Coord current = start;
 
-			for(int i = 1; i <= 3; i++) {
+			for (int i = 1; i <= 3; i++) {
 				current = new Coord(current.getX() + direction.getX(), current.getY() + direction.getY());
-				
+
 				if (current.getX() >= 0 && current.getX() < sizeX && current.getY() >= 0 && current.getY() < sizeY) {
 					if (map.get(current) != XMAS[i]) {
 						break;
@@ -56,9 +56,9 @@ public class DayFour {
 			}
 		}
 		return count;
-	} 
+	}
 
-	public static long partTwo(Map<Coord,Character> map, int sizeX, int sizeY) {
+	public static long partTwo(Map<Coord, Character> map, int sizeX, int sizeY) {
 		long count = 0l;
 		for (int x = 1; x < sizeX - 1; x++) {
 			for (int y = 1; y < sizeY - 1; y++) {
@@ -71,7 +71,7 @@ public class DayFour {
 		return count;
 	}
 
-	public static boolean masInTheShapeOfAnX(Map<Coord,Character> map, final Coord start) {
+	public static boolean masInTheShapeOfAnX(Map<Coord, Character> map, final Coord start) {
 		Character a1 = map.get(new Coord(start.getX() - 1, start.getY() - 1));
 		Character a2 = map.get(new Coord(start.getX() + 1, start.getY() + 1));
 
@@ -91,67 +91,68 @@ public class DayFour {
 		} else if (b1 == 'S' && b2 == 'M') {
 			diagonalB = true;
 		}
-		
+
 		return diagonalA && diagonalB;
 	}
-	
-	private static Map<Coord, Character> parseMap(List<String> lines) {
-        Map<Coord, Character> result = new HashMap<>();
-        for (int y = 0; y < lines.size(); y++) {
-            String line = lines.get(y);
-            for (int x = 0; x < line.length(); x++) {
-                Character shape = line.charAt(x);
-                result.put(new Coord(x, y), shape);
 
-            }
-        }
-        return result;
-    }
+	private static Map<Coord, Character> parseMap(List<String> lines) {
+		Map<Coord, Character> result = new HashMap<>();
+		for (int y = 0; y < lines.size(); y++) {
+			String line = lines.get(y);
+			for (int x = 0; x < line.length(); x++) {
+				Character shape = line.charAt(x);
+				result.put(new Coord(x, y), shape);
+
+			}
+		}
+		return result;
+	}
 
 	private static List<Coord> directions() {
-        List<Coord> result = new ArrayList<>();
-        for (int i = -1; i <= 1 ; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (i != 0 || j != 0) {
-                    result.add(new Coord(i, j));
-                }
-            }
-        }
-        return result;
-    }
+		List<Coord> result = new ArrayList<>();
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (i != 0 || j != 0) {
+					result.add(new Coord(i, j));
+				}
+			}
+		}
+		return result;
+	}
 
 	private static final class Coord {
-        final int x, y;
-        public Coord(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+		final int x, y;
 
-        public int getX() {
-            return x;
-        }
+		public Coord(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 
-        public int getY() {
-            return y;
-        }
+		public int getX() {
+			return x;
+		}
 
-        @Override
-        public String toString() {
-            return "(" + x + ", " + y +")";
-        }
+		public int getY() {
+			return y;
+		}
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
+		@Override
+		public String toString() {
+			return "(" + x + ", " + y + ")";
+		}
 
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof Coord) {
-                Coord other = (Coord) obj;
-                return x == other.x && y == other.y;
-            }
-            return false;
-        }
-    }
+		@Override
+		public int hashCode() {
+			return Objects.hash(x, y);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Coord) {
+				Coord other = (Coord) obj;
+				return x == other.x && y == other.y;
+			}
+			return false;
+		}
+	}
 }
